1279036a84dcd3b6585054591eb79445c1212111
conconcc <lesj9217@gmail.com>
2024-11-29 01:43:56 +0900

    React-Create-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import background from './assets/cctvlocation_image_Designer.jpeg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={background} className="background" alt="background" />
+      
+    </div>
+  );
+}
+
+export default App;
